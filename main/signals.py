037d8b6c58afbb5a172ad591aeada42fb8fcf64e
pokacyb <pokacyb@tutanota.com>
2021-11-22 17:53:23 +0100

    Add comments on main/signals.py

--- main/signals.py
+++ main/signals.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 from . models import UserProfile
 
 
-
+# when an object user is created it sends signals
+# if created this func creates the user profile instance
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
 	if created:

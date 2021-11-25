faa6e6840dff2679dc25f908aba3d2b27e951f73
pokacyb <pokacyb@tutanota.com>
2021-11-25 18:51:55 +0100

    Removing commented urlpatterns which was preventing migrations

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'main'
-"""
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
@@ -11,4 +11,3 @@ urlpatterns = [
     # path('blog/', views.BlogView.as_view(), name='blogs'),
     # path('blog/<slug:slug>', views.BlogDetailView.as_view(), name='blog')
 ]
-"""

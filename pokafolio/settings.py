03e7d313847bd43af8414991227ff1665e83a024
pokacyb <pokacyb@tutanota.com>
2021-12-13 17:20:12 +0100

    Correct static url

--- pokafolio/settings.py
+++ pokafolio/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 
 # New
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'

b820e0734de1e7d9fbe0c202853a02ae05dd4e8b
pokacyb <pokacyb@tutanota.com>
2021-12-21 18:57:53 +0100

    Change root url pattern

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'),
+    path('/', views.IndexView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
     path('portfolio/<slug:slug>/', views.PortfolioDetailView.as_view(), name='portfolio'),

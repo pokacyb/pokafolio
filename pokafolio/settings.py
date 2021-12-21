2456767a8e3044d15cab52e4926f087c22820016
pokacyb <pokacyb@tutanota.com>
2021-12-21 19:05:14 +0100

    debug true

--- pokafolio/settings.py
+++ pokafolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'DEFAULT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pokafolio.herokuapp.com/', '127.0.0.1', 'pokacyb.com/', 'www.pokacyb.com/', 'https://pokacyb.com/']
 
